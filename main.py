fc76422d6a13ca5045be9f05414d52f5499b9164
Snlhajri <Sumianh5@gmail.com>
2023-09-05 02:23:28 +0300

    Cw3.edit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # write your code here
 favorite_animals = ["dog", "cat", "monkey", "rabbit"]
 print(favorite_animals)
-print (favorite_animals([1]))
+print (favorite_animals[1])
 favorite_animals.remove('monkey') 
 print(favorite_animals)
 favorite_animals.append('birds')
@@ -11,5 +11,5 @@ for favorit_animal in favorite_animals:
 numbers= [1,2,3,4,5] 
 numbers_sum= 0
 for sum in numbers:
-    numbers_sum += numbers
+    numbers_sum += sum
     print(numbers_sum)
